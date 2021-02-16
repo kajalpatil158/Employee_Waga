7e3d21efd3113a6955cb42fb957ac79d96b9ed0a
Kajal <kajalpatil158@gmail.com>
2021-02-16 23:41:39 +0530

    Added:Present_or_absent

--- Employee_waga.sh
+++ Employee_waga.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "Well Come to Employee Waga Computation"
+ispresent=1
+
+random=$((RANDOM%2))
+if(($random==$ispresent))
+then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
