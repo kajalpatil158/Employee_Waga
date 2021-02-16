53d322ff3cfd985d4481c03bd7a998edf1eea2e1
Kajal <kajalpatil158@gmail.com>
2021-02-17 03:50:17 +0530

    Added Employee monthaly wages

--- Employee_waga.sh
+++ Employee_waga.sh
@@ -1,33 +1,11 @@
 #!/bin/bash
-
-isPartTime=1
-isFullTime=2
-noOfDays=20
-empRatePerHr=12
-empTotalFullTimeHr=0
-empTotalPartTimeHr=0
-salary=0
-for((i=1;i<$noOfDays;i++))
+totalWorkingHr=100
+totalWorkingDays=20
+for((i=1;i<$totalWorkingHr;i++))
 do
-	randomCheck=$((RANDOM%3))
-	case $randomCheck in
- 		$isFullTime)
-			
-     			empHrs=8
-			empTotalFullTimeHr=$(($empTotalFullTimeHr+$empHrs))
-     			;;
-		$isPartTime)
-     			empHrs=8
-			empTotalPartTimeHr=$(($empTotalPartTimeHr+$empHrs))
-   	  		;;
- 		*)
-     			empHrs=0
-			;;
-	esac
-
-	salary=$(($empHrs*$empRatePerHr+$salary))
+	for((j=1;j<$totalWorkingDays;j++))
+	do
+		wages=$(($totalWorkingHr*$totalWorkingDays))
+	done
 done
-echo "salary: "
-echo $salary
-echo "Employee full time hour:" $empTotalFullTimeHr
-echo "Employee Part time hour is:" $empTotalPartTimeHr
+echo "total working of wages is:" $wages
